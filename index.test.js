eb084abc0b071b8d844b092343ab4bc2b0a4f00a
Jyods <jyodi.sprenger@hotmail.ch>
2024-11-28 11:08:54 +0100

    fixed tests

--- index.test.js
+++ index.test.js
@@ -4,13 +4,13 @@ import { sum } from "./index.js";
 describe("sum", () => {
   it("should return 3 for 1 + 2", () => {
     // Korrigieren Sie den Test
-    assert.equal(sum(1, 2), 4);
+    assert.equal(sum(1, 2), 3);
   });
 });
 
 describe("decimals", () => {
-    it("should return 0.3 for 0.1 + 0.2", () => {
+    it("should return 0.3 for 0.1 + 0.4", () => {
         // Korrigieren Sie den Test
-        assert.equal(0.1 + 0.2, 0.3);
+        assert.equal(0.1 + 0.4, 0.5);
     });
 });
